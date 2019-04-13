41b46d9fdf6c6aa59e299652626bc7b555a255f0
Jason Wang <wang.jason.010@gmail.com>
2019-04-13 12:52:23 -0700

    First commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,95 @@
+const mongoose = require("mongoose"),
+  User = mongoose.model("User"),
+  Score = mongoose.model("Score"),
+  bcrypt = require("bcrypt");
+
+module.exports = {
+  userIndex: (req, res) => {
+    User.find({})
+      .then(data => {
+        res.json({ message: "Success", data: data });
+      })
+      .catch(err => {
+        res.json({ message: "Error", error: err });
+      });
+  },
+
+  userShow: (req, res) => {
+    User.find({ _id: req.body.id })
+      .then(data => {
+        res.json({ message: "Success", data: data });
+      })
+      .catch(err => {
+        res.json({ message: "Error", error: err });
+      });
+  },
+
+  userNew: (req, res) => {
+    console.log("Controller check", req.body);
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    var user = new User({
+      username: req.body.name,
+      password: hash
+    });
+    user
+      .save()
+      .then(item => {
+        res.json({ message: "Success", item });
+      })
+      .catch(err => {
+        res.json({ message: "Failed!", err });
+      });
+  },
+
+  userLogin: (req, res) => {
+    User.find({ username: req.body.name }, function(err, user) {
+      if (err) {
+        res.redirect("/");
+      } else if (user) {
+        console.log(user);
+        console.log(user[0].password);
+        bcrypt
+          .compare(req.body.password, user[0].password)
+          .then(data => {
+            console.log("data looks like this:", data);
+            if (req.session.loginID) {
+              res.json({ message: "Success", data: user[0] });
+            } else {
+              req.session.loginID = user[0]._id;
+              res.json({ message: "Success", data: user[0] });
+            }
+          })
+          .catch(err => {
+            res.json({ message: "Error", error: err });
+          });
+      }
+    });
+  },
+
+  getLogin: (req, res) => {
+    User.find({ _id: req.session.loginID }, function(err, user) {
+      if (err) {
+        res.redirect("/");
+      } else if (user) {
+        if (user.length < 1) {
+          var temp = null;
+          res.json(temp);
+        } else {
+          res.json(user);
+        }
+      }
+    });
+  },
+
+  logout: (req, res) => {
+    if (req.session.loginID) {
+      req.session.loginID = null;
+      res.json(req.session.loginID);
+    }
+  },
+
+  score: (req, res) => {
+    Score.find({ _id: req.params.id });
+  }
+};
---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      Required: true,
+      minlength: [3, "Username must be longer than 3 characters"],
+      maxlength: [16, "Username cannot be more than 16 characters long"]
+    },
+    email_address: { type: mongoose.SchemaTypes.Email, required: true },
+    password: { type: String },
+    first_name: { type: String },
+    last_name: { type: String },
+    dob: { type: Date },
+    zip_code: { type: Number },
+    city: { type: String },
+    state: { type: String },
+    street_one: { type: String },
+    street_two: { type: String },
+    phone_number: { type: Number },
+    loan_amount: { type: Number },
+    loan_duration_months: { type: Number },
+    employment_status: { type: String },
+    gross_annual_income: { type: String },
+    net_annual_income: { type: Number },
+    pay_frequency_weeks: { type: Number }
+  },
+  { timestamps: true }
+);
+var User = mongoose.model("User", UserSchema);
+
+const ScoreSchema = new mongoose.Schema(
+  {
+    score: { type: Number },
+    user: [UserSchema]
+  },
+  { timestamps: true }
+);
+var Score = mongoose.model("Score", ScoreSchema);
+
+module.exports = [User, Score];
